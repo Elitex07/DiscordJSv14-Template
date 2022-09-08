3b1ae3c36a67c8a68be9a5dc02ca541ad85ec3e8
Priyanshu Jain <jain.yashu2211@gmail.com>
2022-09-08 07:11:00 +0530

    Update connect.js

--- Database/connect.js
+++ Database/connect.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
 async function connect() {
+    if(!process.env.mongo){
+        console.warn(`[DATABASE] For database access, MongoDB URI, is required in .env file.`)
+        return
+    }
     mongoose.connect(process.env.mongo, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
 
     mongoose.connection.once("open", () => {
-        console.log('[DATABASE]: Connected To Database')
+        console.log('[DATABASE] Connected To Database')
     })
     return;
 }
